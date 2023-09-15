cd61ef4536bcb5a4d803eb5abcfb6b7b1caf005d
Stefanolr04 <136249681+Stefanolr04@users.noreply.github.com>
2023-09-15 16:26:17 +0000

    ig data modeling

--- src/models.py
+++ src/models.py
@@ -25,8 +25,6 @@ class Post(Base):
 class Media(Base):
     __tablename__='media'
     id = Column(Integer, primary_key=True)
-    type = Column(enum)
-    type = Column(enumerate)
     ulr = Column(String)
     post_id = Column(Integer, ForeignKey('post.id'))
     post = relationship(Post)
